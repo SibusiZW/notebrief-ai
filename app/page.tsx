6032d84ea7a3a3b9e3d6600c1aa13e254888c6f4
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-18 11:56:33 +0200

    added initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { BookText, Scissors } from "lucide-react";
+
 export default function HomePage() {
   return (
-    <div></div>
+    <div className="space-y-8 p-4 flex flex-col items-center justify-center">
+      <div className="flex mb-2 text-gray-500">
+        <BookText />
+        Enter you notes here
+      </div>
+
+      <div className="flex flex-col p-2 w-full max-w-[450px]">
+        <form>
+          <Textarea required className="mb-2"/>
+          <Button type="submit" className="w-full bg-indigo-500 text-white">
+            <Scissors />
+            Summarize Now
+          </Button>
+        </form>
+      </div>
+    </div>
   )
 }
